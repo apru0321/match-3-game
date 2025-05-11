a29fa4c450995089de41e42ff74a2a302bfe02f5
apru0321 <apru0321@gmail.com>
2025-05-11 14:40:40 +0300

    Create constants.js

---
+++ src/js/constants.js
@@ -0,0 +1,16 @@
+export const GRID_WIDTH = 6;
+export const GRID_HEIGHT = 6;
+export const ALL_SHAPES = ['square', 'circle', 'triangle'];
+export const ALL_COLORS = ['#ff5555', '#55ff55', '#5555ff'];
+export const PREDEFINED_TASKS = [
+    { shape: 'square', count: 10, moves: 3 },
+    { shape: 'circle', count: 12, moves: 3 },
+    { shape: 'triangle', count: 8, moves: 2 },
+    { shape: 'square', count: 15, moves: 3 },
+    { shape: 'circle', count: 10, moves: 2 },
+    { shape: 'triangle', count: 14, moves: 3 },
+    { shape: 'square', count: 12, moves: 4 },
+    { shape: 'circle', count: 16, moves: 3 },
+    { shape: 'triangle', count: 10, moves: 2 },
+    { shape: 'square', count: 18, moves: 3 }
+];
